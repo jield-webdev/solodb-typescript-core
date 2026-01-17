8375dcaa65da6f6bed24e7ea9ab5b1f7948a0e62
Johan van der Heide <johan.vanderheide@jield.nl>
2026-01-17 16:40:40 +0100

    Add a license

--- src/equipment/interfaces/setup.ts
+++ src/equipment/interfaces/setup.ts
@@ -1,10 +1,27 @@
-import { SetupEquipment } from "@jield/solodb-typescript-core/equipment/interfaces/setup/equipment";
-import { Zone } from "@jield/solodb-typescript-core/room/interfaces/zone";
+import {SetupEquipment} from "@jield/solodb-typescript-core/equipment/interfaces/setup/equipment";
+import {Zone} from "@jield/solodb-typescript-core/room/interfaces/zone";
+import {Room} from "@jield/solodb-typescript-core/room/interfaces/room";
+import {Equipment} from "@jield/solodb-typescript-core/equipment/interfaces/equipment";
+
+export enum SetupTypeEnum {
+    "FIXED" = 1,
+    "DYNAMIC" = 2,
+}
+
+export enum SetupReservationTypeEnum {
+    "NO_RESERVATION_POSSIBLE" = 1,
+    "SCHEDULER" = 2,
+    "CALENDAR" = 3,
+}
 
 export type Setup = {
-  id: number;
-  title: string;
-  description: string;
-  zone: Zone;
-  setup_equipment: SetupEquipment[];
+    id: number;
+    setup_type: SetupTypeEnum;
+    reservation_type: SetupReservationTypeEnum;
+    main_equipment: Equipment
+    zone: Zone;
+    room: Room;
+    setup_equipment: SetupEquipment[];
 };
+
+export type SetupWithoutMainEquipmentAndSetupEquipment = Omit<Setup, 'main_equipment' | 'setup_equipment'>;
