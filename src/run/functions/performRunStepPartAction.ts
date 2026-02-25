6e89acdd21afaad7c607ca791cf118ecc0bc6e5d
pedrofm <pedrofm@pedrofm.dev>
2026-02-25 15:14:27 +0100

    Fix yarn lint issue

--- src/run/functions/performRunStepPartAction.ts
+++ src/run/functions/performRunStepPartAction.ts
@@ -4,16 +4,19 @@ import { RunStepPartActionEnum } from "../enum/runStepPartActionEnum";
 import { RunStepPart } from "../interfaces/step/runStepPart";
 
 /*
- * Performs a action in a runStepPart 
+ * Performs a action in a runStepPart
  * Also it makes sure the step state is what it should be
  */
-export default async function performRunStepPartAction(part: RunStepPart, action: RunStepPartActionEnum) {
-    const step = part.step;
+export default async function performRunStepPartAction(
+  part: RunStepPart,
+  action: RunStepPartActionEnum,
+): Promise<void> {
+  const step = part.step;
 
-    // start step if it isnt 
-    if (!step.is_started) {
-        startStep(step); 
-    }
-   
-    return setRunStepPartAction({runStepPart: part, runStepPartAction: action});
+  // start step if it isnt
+  if (!step.is_started) {
+    await startStep(step);
+  }
+
+  await setRunStepPartAction({ runStepPart: part, runStepPartAction: action });
 }
