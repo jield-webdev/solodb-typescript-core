207afcb33764485d0cb6b7eeb256dde970c2e0d6
Johan van der Heide <johan.vanderheide@jield.nl>
2026-03-26 21:26:20 +0100

    Updated the list of RunStepPartActionEnum

--- src/run/enum/runStepPartActionEnum.ts
+++ src/run/enum/runStepPartActionEnum.ts
@@ -1,6 +1,8 @@
 export enum RunStepPartActionEnum {
-  START_PROCESSING = 1,
-  FINISH_PROCESSING = 2,
-  FAILED_PROCESSING = 3,
-  REWORK = 4,
+    START_PROCESSING = 1,
+    FINISH_PROCESSING = 2,
+    FAILED_PROCESSING = 3,
+    REWORK = 4,
+    REPAIR = 5,
+    TESTING = 6
 }
