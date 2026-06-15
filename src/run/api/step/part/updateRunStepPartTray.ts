270cad442ecf32d7c2fa4a869f59a1bf65ece9b7
pedrofm <pedrofm@pedrofm.dev>
2026-06-15 10:38:22 +0200

    Sync updateRunStePartTray fn

--- src/run/api/step/part/updateRunStepPartTray.ts
+++ src/run/api/step/part/updateRunStepPartTray.ts
@@ -7,18 +7,25 @@ export default async function updateRunStepPartTray(
   tray: RunTray,
   tray_row: number,
   tray_column: number,
+  update_subsequent: boolean | null,
 ): Promise<AxiosResponse<RunStepPart>> {
   if (!stepPart || !tray || !tray_column || !tray_row) {
     throw new Error("Invalid values when trying to update run step part tray");
   }
 
+  const body: any = {
+    tray_id: tray.id,
+    tray_row: tray_row,
+    tray_column: tray_column,
+  };
+
+  if (update_subsequent !== null) {
+    body.update_subsequent = update_subsequent;
+  }
+
   const resp = await axios
     .create()
-    .patch<RunStepPart>(`update/run/step/part/${stepPart.id}/tray`, {
-        tray_id: tray.id,
-        tray_row: tray_row,
-        tray_column: tray_column,
-    });
+    .patch<RunStepPart>(`update/run/step/part/${stepPart.id}/tray`, body);
 
   return resp;
 }
